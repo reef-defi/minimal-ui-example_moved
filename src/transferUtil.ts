e96bbfa085bc581fa59e3c4fce9f72bb42cf749b
Matjaz Hirsman <matjazonline@gmail.com>
2023-03-14 09:49:50 +0100

    complete method call example

--- src/transferUtil.ts
+++ src/transferUtil.ts
@@ -3,6 +3,22 @@ import {transactionUtils} from "@reef-chain/util-lib";
 import {Contract} from "ethers";
 import {Observable} from "rxjs";
 import {TransactionStatusEvent} from "../../reef-util-lib/lib/transaction";
+import {initProvider} from "./providerUtil";
+import {web3Enable} from "@reef-defi/extension-dapp";
+import {REEF_EXTENSION_IDENT} from "@reef-defi/extension-inject";
+
+const ERC20_TRANSFER_ABI = [{
+    inputs: [
+        { name: 'recipient', type: 'address' },
+        { name: 'amount', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+}];
+
+const STORAGE_LIMIT = 2000;
 
 export async function sendNativeREEFTransfer (amount: string, fromSigner: Signer, toAddress: string): Promise<void>{
     await transactionUtils.nativeTransferSigner$(amount, fromSigner, toAddress).subscribe((val: any) => {
@@ -11,16 +27,19 @@ export async function sendNativeREEFTransfer (amount: string, fromSigner: Signer
 }
 
 export  function sendERC20Transfer (amount: string, fromSigner: Signer, toAddress: string, contractAddress: string): Observable<TransactionStatusEvent>{
-    const ERC20 = [{
-        inputs: [
-            { name: 'recipient', type: 'address' },
-            { name: 'amount', type: 'uint256' },
-        ],
-        name: 'transfer',
-        outputs: [{ name: '', type: 'bool' }],
-        stateMutability: 'nonpayable',
-        type: 'function',
-    }];
-    let tContract=new Contract(contractAddress, ERC20, fromSigner);
+    let tContract=new Contract(contractAddress, ERC20_TRANSFER_ABI, fromSigner);
     return transactionUtils.reef20Transfer$(toAddress, fromSigner.provider, amount, tContract);
+}
+
+async function completeTransferExample(amount: string, fromAddr: string, toAddress: string, contractAddress: string): Promise<any>{
+    const extensionsArr = await web3Enable('Test Transfer');
+    const extension = extensionsArr.find(e=>e.name===REEF_EXTENSION_IDENT);
+    const provider = await initProvider('wss://rpc.reefscan.info/ws');
+    const signer = new Signer(provider, fromAddr, extension.signer);
+    const tokenContract = new Contract(contractAddress, ERC20_TRANSFER_ABI, signer);
+    return tokenContract.transfer(toAddress, amount, {
+        customData: {
+            storageLimit: STORAGE_LIMIT
+        }
+    });
 }
